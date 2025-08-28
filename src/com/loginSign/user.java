09c2dcb083b085421cb2b9e976de99abc52bd36b
devsanandiya <bansipan1234@gmail.com>
2025-08-28 18:43:48 +0530

    new class add

---
+++ src/com/loginSign/user.java
@@ -0,0 +1,33 @@
+package com.loginSign;
+
+public class user {
+	
+	private String username;
+	private String Password;
+	private String phonenumber;
+	private String email;
+	
+	public user(String username,String Password,String phonenumber,String email) {
+		
+		this.username=username;
+		this.Password=Password;
+		this.phonenumber=phonenumber;
+		this.email=email;
+	}
+	public String getusername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return Password;
+	}
+	
+	public String getphonenumber() {
+		return phonenumber;
+	}
+	
+	public String getemail() {
+		return email;
+	}
+
+}
