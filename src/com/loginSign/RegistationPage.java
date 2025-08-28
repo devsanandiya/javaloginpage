95982f2143bc49b4d73f1634b37637305a523f90
Dev Sanandiya <bansipan1234@gmail.com>
2025-08-28 19:34:30 +0530

    add files

--- src/com/loginSign/RegistationPage.java
+++ src/com/loginSign/RegistationPage.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class RegistationPage {
 	public static void main(String[]args) {
 		Scanner sc=new Scanner(System.in);
+		String Y;
+		do {
 		System.out.println("Enter Your Username");
 			String username=sc.nextLine();
 		System.out.println("Enter Your Email");
@@ -13,24 +15,34 @@ public class RegistationPage {
 		    String password=sc.nextLine();
 		System.out.println("Enter Your Confirm Password");
 		    String confiPassword=sc.nextLine();
-		if(username!=null&&password!=null&&email!=null &&phone==null&&confiPassword!=null) {
+		if(username!=null&&password!=null&&email!=null &&phone!=null&&confiPassword!=null) {
 			System.out.println("Successfull in first check phase");
 			if(email.contains("@")&&email.contains(".com")) {
 				System.out.println("Email Successfull");
 				if(phone.charAt(0)>6&&phone.charAt(0)<9) {
-					System.out.println("Success Number");{
+					System.out.println("Success Number");
+					if(confiPassword==password) {
+						System.out.println("All Step Done ");
+						SingUp sig=new SingUp(username ,email,phone,password);
+					}
+					else {
+						System.out.println("Error check id pass all");
+					}
+			
+				}
+				else {
+					System.out.println("Check All Thing Phone Number Invalid");
 				}
 			}
+			else {
+				System.out.println("ERROR:check email invalid");
+			}
 		}
+		else {
+			System.out.println("Invalid input enetered");
+		}
+		System.out.println("Do You Want To Countinue?");
+		Y=sc.nextLine();
+		}while(Y.equalsIgnoreCase("y"));
 	}
-public class SingUp {
-		public SingUp() 
-			{
-				System.out.println("invalid inputted");
-			}
-			public SingUp (String username,String email,String phone,String password) {
-				System.out.println("SuccessFully");
-			}
-		
-}
 }
---
+++ src/com/loginSign/SingUp.java
@@ -0,0 +1,12 @@
+package com.loginSign;
+
+public class SingUp {
+	public SingUp() 
+	{
+		System.out.println("invalid inputted");
+	}
+	public SingUp (String username,String email,String phone,String password) {
+		System.out.println("SuccessFully");
+	}
+
+}
