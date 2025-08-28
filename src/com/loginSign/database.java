c556781bf77c359df79ecf0e519bf9a55d417848
devsanandiya <bansipan1234@gmail.com>
2025-08-28 23:43:46 +0530

    add databse

--- src/com/loginSign/database.java
+++ src/com/loginSign/database.java
@@ -9,9 +9,42 @@ public class database {
      
 	public void  adduser(User user) {
          users.add(user);
-         
-         
-		
+         System.out.println("user added sucessfully");
+ 	}
+	
+	public boolean validuser(String username) {
+		for(User u :users) {
+			if(u.getusername().equals(username)) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
+	public boolean validpassword(String password) {
+		for (User u : users) {
+			if(u.getPassword().equals(password)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean validphonenumber(String phonenumber) {
+		for(User u :users) {
+			if(u.getphonenumber().equals(phonenumber)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean validemail(String email) {
+		for(User u : users) {
+			if(u.getemail().equals(email)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
