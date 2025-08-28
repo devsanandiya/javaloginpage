ab24da00afdf30790501f904a42997ab54382457
Dev Sanandiya <bansipan1234@gmail.com>
2025-08-28 17:09:07 +0530

    new file add

---
+++ src/com/loginSign/Login.java
@@ -0,0 +1,16 @@
+package com.loginSign;
+
+public class Login {
+		String username;
+		String password;
+			public Login(){
+				System.out.println("Invalid input");
+			}
+			public Login(String username) {
+				System.out.println("Password Must Be Required");
+			}
+			public Login(String username,String password) {
+				this.username=username;
+				this.password=password;
+			}
+}
---
+++ src/com/loginSign/RegistationPage.java
@@ -0,0 +1,36 @@
+package com.loginSign;
+import java.util.Scanner;
+public class RegistationPage {
+	public static void main(String[]args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Your Username");
+			String username=sc.nextLine();
+		System.out.println("Enter Your Email");
+			String email=sc.nextLine();
+		System.out.println("Enter Your Phone Number");
+			String phone=sc.nextLine();
+		System.out.println("Enter Your Password");
+		    String password=sc.nextLine();
+		System.out.println("Enter Your Confirm Password");
+		    String confiPassword=sc.nextLine();
+		if(username!=null&&password!=null&&email!=null &&phone==null&&confiPassword!=null) {
+			System.out.println("Successfull in first check phase");
+			if(email.contains("@")&&email.contains(".com")) {
+				System.out.println("Email Successfull");
+				if(phone.charAt(0)>6&&phone.charAt(0)<9) {
+					System.out.println("Success Number");{
+				}
+			}
+		}
+	}
+public class SingUp {
+		public SingUp() 
+			{
+				System.out.println("invalid inputted");
+			}
+			public SingUp (String username,String email,String phone,String password) {
+				System.out.println("SuccessFully");
+			}
+		
+}
+}
